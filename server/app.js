0766a4a5211279764a94c50ae96ed7abdc552e99
amanpatel3045 <amanpatel3045@gmail.com>
2022-11-28 01:58:54 +0530

    middleware

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 // app k andr express ke sare functions and properties aa chuke hai bcz const app=express() likha hai
 const app = express();
-//line no 5 home page ko represent karega
+
+//MIDDLEWARE
+
+const middleware = (req, res, next) => {
+  // next parameter means=> middleware ka kaam ho jane k baad next jo bhi hai wo kaam shuru kr do
+  console.log(`I am Middleware`);
+  next();
+};
+
+//bellow line home page ko represent karega
 app.get("/", (req, res) => {
   //res.send me jo likha hai wo home page pe show hoga
   res.send(`Hello world from the server, you are on Home Page`);
@@ -10,33 +19,33 @@ app.get("/", (req, res) => {
 //MIDDLEWARE=> user about page ko jb bhi access karega yaa about icon pe click karega toh
 //Middleare check karega ki user logged in hai ya nhi agr user already loggedin hai
 //toh wo about page ko access karega agr user new hai ya loggedin nhi hai
-//toh use signup yaa login page pe redirect kr denge 
+//toh use signup yaa login page pe redirect kr denge
 //then user signup/login krne k baad about page access kr skta hai but usme keval particular user
 //jo user login kr rha hai usi ki details about page me dikhegi na ki dusre ki details
 //that's why yeh ek middle ki trh work kr rha hai
 
-
 //ALL PAGES =>HOME,ABOUT,CONTACT,SIGNIN,SIGNUP KO BACKEND KI HELP SE CREATE KRO.....
 // now about page ke liye app.get likho
-app.get("/about",(req,res)=>{
-    res.send(`You are on about page`);
+app.get("/about", middleware, (req, res) => {
+  console.log(`after executing middleware`);
+  res.send(`You are on about page`);
 });
 
-//now contact page k liye app.get likho taaki user /contact pe jaye toh use mera 
+//now contact page k liye app.get likho taaki user /contact pe jaye toh use mera
 //res.get me jp likha hai wo dikhe
 
-app.get("/contact",(req,res)=>{
-    res.send(`You are on contact page`);
+app.get("/contact", (req, res) => {
+  res.send(`You are on contact page`);
 });
 
 //now signin page ke liye
-app.get("/signin",(req,res)=>{
-    res.send(`You are on sign in page`);
+app.get("/signin", (req, res) => {
+  res.send(`You are on sign in page`);
 });
 
 //now signup page ke liye
-app.get("/signup",(req,res)=>{
-    res.send(`You are on signup page`);
+app.get("/signup", (req, res) => {
+  res.send(`You are on signup page`);
 });
 
 // now server ko btaao ki user / wale page pe visit kr rha hai
